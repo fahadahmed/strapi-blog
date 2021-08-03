9c682dfd261e1ed85e9fa51cb388ef2f7859e5df
Fahad Ahmed <fahad.ahmed@me.com>
2021-08-03 22:22:42 +1000

    CP-4 deployed the app via Google Cloud Run

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
+function getDevConnections(env) {
+  return {
     default: {
       connector: 'bookshelf',
       settings: {
@@ -11,5 +10,34 @@ module.exports = ({ env }) => ({
         useNullAsDefault: true,
       },
     },
-  },
+  }
+}
+
+function getProductionConnections(env) {
+  return {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+      },
+      options: {
+        pool: {
+          min: 0,
+          max: 15,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000,
+        },
+      },
+    },
+  }
+}
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: env('NODE_ENV') === 'development' ? getDevConnections(env) : getProductionConnections(env)
 });
